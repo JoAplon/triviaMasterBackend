c6cdf2a808d979707ff3f1cb49b83634ada6a75b
jasondang4 <jason1094dang@gmail.com>
2024-04-25 10:10:39 -0700

    backend code

---
+++ routes/gameRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const Game = require('../models/Game');
+const axios = require('axios');
+const router = express.Router();
+
+// Add routes for starting a game, fetching questions from the API, and saving game results
+
+module.exports = router;
